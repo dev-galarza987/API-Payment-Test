308a0a8e3b2ea6ca7c25ea9b8eadf08fa93c7d2e
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-10-30 23:44:15 -0400

    config(api): added configurations for the API.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PREFIX_API = process.env.APP_PREFIX || 'api';
+  const VERSION_API = process.env.API_VERSION || 'v1';
+  app.enableCors();
+  app.setGlobalPrefix(PREFIX_API + '/' + VERSION_API);
+  const PORT = process.env.PORT || 3000;
+  app.use(morgan('dev'));
+  await app.listen(PORT, () => {
+    console.log(
+      `Server listening on http://${process.env.APP_HOST}:${PORT}/${PREFIX_API}/${VERSION_API}`,
+    );
+  });
 }
-bootstrap();
+void bootstrap();
